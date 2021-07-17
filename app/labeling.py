ade309fe0722a31b008a6e1f971a726edd266185
Maxim Derkachev <max.derkachev@gmail.com>
2021-07-17 15:39:42 +0300

    refactor labeling app

--- app/labeling.py
+++ app/labeling.py
@@ -1,12 +1,15 @@
-from typing import List, Set, Dict
-import numpy as np
+from app.utils.labeling import (
+    ModelArtifact,
+    ModelArtifactMetadata,
+    prepareArtifact,
+    processText,
+    walkTree,
+)
 import pandas as pd
 import streamlit as st
-import torch
-from nltk.tokenize import sent_tokenize
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from annotated_text import annotated_text
-from anytree import Node, RenderTree, AsciiStyle, PreOrderIter
+from anytree import RenderTree, AsciiStyle
 from docx import Document
 from pathlib import Path
 from app.utils.color import getColorForText
@@ -19,13 +22,6 @@ st.set_page_config(
 DATA_DIR = Path.cwd() / "data"
 CHECKPOINT = DATA_DIR / "models" / "checkpoints" / "checkpoint-2500"
 
-tokenizer = model = None
-LABELS_DICT: Dict[int, str] = {}
-TAGS_MAP: Dict[str, str] = {}
-TAGS: Dict[str, str] = {}
-
-IGNORE_LABELS = {"O", "B-MISC", "I-MISC"}
-
 DEFAULT_TEXT = "Germany's representative to the European Union's veterinary committee Werner Zwingmann said on Wednesday consumers should buy sheepmeat from countries other than Britain until the scientific advice was clearer."
 
 
@@ -37,19 +33,10 @@ def getTag(label):
 def initialize(model_checkpoint: str):
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
     model = AutoModelForTokenClassification.from_pretrained(model_checkpoint)
-
-    config = model.config
-
-    LABELS_DICT = {int(k): v for (k, v) in config.id2label.items()}
-
-    TAGS_MAP = {l: getTag(l) for l in LABELS_DICT.values() if l not in IGNORE_LABELS}
-
-    TAGS = {t: getColorForText(t) for t in TAGS_MAP.values()}
-
-    return tokenizer, model, LABELS_DICT, TAGS_MAP, TAGS
+    return prepareArtifact(tokenizer, model, tagMapper=getColorForText)
 
 
-tokenizer, model, LABELS_DICT, TAGS_MAP, TAGS = initialize(str(CHECKPOINT))
+modelArtifact = initialize(str(CHECKPOINT))
 
 
 def readDocx(file):
@@ -82,164 +69,17 @@ text: str = st.text_area(
     height=100,
 )
 
-
-@st.cache(allow_output_mutation=True, show_spinner=False)
-def processText(s: str):
-    global tokenizer, model
-
-    with st.spinner(text="Labeling sequence:"):
-        sentences = sent_tokenize(s)
-
-        special_ids = set(tokenizer.all_special_ids)
-        # tis = [
-        #     tokenizer.encode(sent, padding="max_length") for sent in sentences
-        # ]
-        # input_ids = torch.tensor(tis)
-        # logits = model(input_ids).logits
-
-        trees = []
-        globalOffset = 0
-        for i, sent in enumerate(sentences):
-            ti = tokenizer.encode(sent)
-            input_ids = torch.tensor([ti])
-            logit = model(input_ids).logits[0]
-            # ti = tis[i]
-            # logit = logits[i]
-            labels = np.argmax(logit.detach().numpy(), axis=1)
-            tokens = tokenizer.convert_ids_to_tokens(ti)
-            globalOffset = s.index(sent, globalOffset)
-            trees.append(makeTree(sent, ti, tokens, labels, special_ids, globalOffset))
-        return trees, sentences
-
-
-def makeTree(
-    sentence: str,
-    ti: List[int],
-    tokens: List[str],
-    labels: List[int],
-    special_ids: Set[int],
-    globalOffset: int = 0,
-):
-    sLower = sentence.lower()
-    root = Node(sentence, type="root", globalOffset=globalOffset)
-
-    lastTag = None
-    lastWord = None
-
-    next = 0
-
-    for i, t, l in zip(ti, tokens, labels):
-        if i in special_ids:
-            continue
-
-        continuation = False
-
-        if t.startswith("##"):
-            continuation = True
-            t = t[2:]
-
-        pos = sLower.find(t[0], next)
-        if pos == -1:
-            raise Exception(f"Can't find '{t}' from position {next}")
-        if pos > 0 and pos == next:
-            continuation = True
-        next = pos + len(t)
-        word = sentence[pos:next]
-
-        label = LABELS_DICT[l]
-        tag = TAGS_MAP.get(label) or label
-        if not lastTag or tag != lastTag.tag:
-            lastTag = Node(f"[{tag}]", type="tag", tag=tag, parent=root)
-            lastWord = Node(
-                f"{'##' if continuation else ''}{word} [{label}] [{pos + globalOffset}:{next + globalOffset}]",
-                type="word",
-                word=word,
-                s=pos + globalOffset,
-                e=next + globalOffset,
-                cont=continuation,
-                label=label,
-                parent=lastTag,
-            )
-        else:
-            parent = None
-            if label != tag:
-                if lastWord:
-                    if label.startswith("I-") and lastWord.label.startswith(
-                        ("B-", "I-")
-                    ):
-                        parent = lastTag
-                    elif label == lastWord.label and continuation:
-                        parent = lastWord
-                    else:
-                        parent = lastTag = Node(
-                            f"[{tag}]", type="tag", tag=tag, parent=root
-                        )
-                else:
-                    parent = lastTag
-            else:
-                if continuation and lastWord:
-                    parent = lastWord
-                else:
-                    parent = lastTag = Node(
-                        f"[{tag}]", type="tag", tag=tag, parent=root
-                    )
-
-            lastWord = Node(
-                f"{'##' if continuation else ''}{word} [{label}] [{pos + globalOffset}:{next + globalOffset}]",
-                type="word",
-                word=word,
-                s=pos + globalOffset,
-                e=next + globalOffset,
-                cont=continuation,
-                label=label,
-                parent=parent,
-            )
-    return root
-
-
-def walkTree(tree):
-    annotated = []
-    interesting = []
-    offset = tree.globalOffset
-    for t in PreOrderIter(tree, maxlevel=2, filter_=lambda n: n.type == "tag"):
-        words = []
-        start, end = -1, -1
-        for w in PreOrderIter(t, filter_=lambda n: n.type == "word"):
-            words.append(text[offset : w.s])
-            if start == -1:
-                start = w.s
-            end = offset = w.e
-
-            words.append(w.word)
-
-        coloring = TAGS.get(t.tag)
-        if coloring:
-            subString = "".join(words)
-            tagged = (subString, t.tag, coloring)
-            # try:
-            #     index = s.index(subString, start - 1)
-            # except:
-            #     st.write(subString, start, end)
-            #     raise
-            # if index != start:
-            #     st.write(subString, "Expected:", start, "Actual:", index)
-            annotated.append(tagged)
-            interesting.append((tagged, (start, end)))
-        else:
-            annotated.append("".join(words))
-
-    return annotated, interesting
-
-
-trees, sentences = processText(text)
+with st.spinner(text="Labeling sequence:"):
+    trees, sentences = processText(text, modelArtifact)
 
 with st.spinner(text="Rendering results..."):
     if st.sidebar.checkbox("Show sentences"):
         st.write("Sentences:", sentences)
 
     annotated, interesting = [], []
+    tags = modelArtifact["metadata"]["tags"]
     for tree in trees:
-        a, i = walkTree(tree)
+        a, i = walkTree(text, tree, tags)
         annotated.extend(a)
         annotated.append(" ")
         interesting.extend(i)
---
+++ app/utils/labeling.py
@@ -0,0 +1,209 @@
+from typing import Any, Dict, List, Optional, Set, TypedDict
+
+from anytree import Node, PreOrderIter
+from nltk.tokenize import sent_tokenize
+import torch
+import numpy as np
+
+
+def getTag(label):
+    return label.replace("B-", "").replace("I-", "")
+
+
+class ModelArtifactMetadata(TypedDict):
+    ignoreLabels: Set[str]
+    labelsDict: Dict[int, str]
+    tagsMap: Dict[str, str]
+    tags: Dict[str, str]
+    specialIds: Set[int]
+
+
+class ModelArtifact(TypedDict):
+    model: Any
+    tokenizer: Any
+    metadata: ModelArtifactMetadata
+
+
+def prepareArtifact(tokenizer, model, tagMapper=lambda tag: tag):
+    config = model.config
+
+    ignoreLabels = {"O", "B-MISC", "I-MISC"}
+    labelsDict = {k: v for (k, v) in config.id2label.items()}
+    tagsMap = {l: getTag(l) for l in labelsDict.values() if l not in ignoreLabels}
+    tags = {t: tagMapper(t) for t in tagsMap.values()}
+    specialIds = set(tokenizer.all_special_ids)
+
+    return ModelArtifact(
+        tokenizer=tokenizer,
+        model=model,
+        metadata=ModelArtifactMetadata(
+            ignoreLabels=ignoreLabels,
+            labelsDict=labelsDict,
+            tagsMap=tagsMap,
+            tags=tags,
+            specialIds=specialIds,
+        ),
+    )
+
+
+def processText(s: str, modelArtifact: ModelArtifact):
+    sentences = sent_tokenize(s)
+
+    # tis = [
+    #     tokenizer.encode(sent, padding="max_length") for sent in sentences
+    # ]
+    # input_ids = torch.tensor(tis)
+    # logits = model(input_ids).logits
+
+    trees = []
+    globalOffset = 0
+    for i, sent in enumerate(sentences):
+        trees.append(processSentence(sent, modelArtifact, s, globalOffset))
+    return trees, sentences
+
+
+def processSentence(sentence, modelArtifact: ModelArtifact, text=None, globalOffset=0):
+    tokenizer = modelArtifact["tokenizer"]
+    model = modelArtifact["model"]
+    metadata = modelArtifact["metadata"]
+    ti = tokenizer.encode(sentence)
+    input_ids = torch.tensor([ti])
+    logit = model(input_ids).logits[0]
+    # ti = tis[i]
+    # logit = logits[i]
+    labels = np.argmax(logit.detach().numpy(), axis=1)
+    tokens = tokenizer.convert_ids_to_tokens(ti)
+    if text:
+        globalOffset = text.index(sentence, globalOffset)
+    metadata = metadata
+    return makeTree(
+        sentence,
+        ti,
+        tokens,
+        labels,
+        metadata["specialIds"],
+        metadata["labelsDict"],
+        metadata["tagsMap"],
+        globalOffset,
+    )
+
+
+def makeTree(
+    sentence: str,
+    ti: List[int],
+    tokens: List[str],
+    labels: List[int],
+    specialIds: Set[int],
+    labelsDict: Dict[int, str],
+    tagsMap: Dict[str, str],
+    globalOffset: int = 0,
+):
+    sLower = sentence.lower()
+    root = Node(sentence, type="root", globalOffset=globalOffset)
+
+    lastTag: Optional[Node] = None
+    lastWord: Optional[Node] = None
+
+    next = 0
+
+    for i, t, l in zip(ti, tokens, labels):
+        if i in specialIds:
+            continue
+
+        continuation = False
+
+        if t.startswith("##"):
+            continuation = True
+            t = t[2:]
+
+        pos = sLower.find(t[0], next)
+        if pos == -1:
+            raise Exception(f"Can't find '{t}' from position {next}")
+        if pos > 0 and pos == next:
+            continuation = True
+        next = pos + len(t)
+        word = sentence[pos:next]
+
+        label = labelsDict[l]
+        tag = tagsMap.get(label) or label
+        if not lastTag or tag != lastTag.tag:
+            lastTag = Node(f"[{tag}]", type="tag", tag=tag, parent=root)
+            lastWord = Node(
+                f"{'##' if continuation else ''}{word} [{label}] [{pos + globalOffset}:{next + globalOffset}]",
+                type="word",
+                word=word,
+                s=pos + globalOffset,
+                e=next + globalOffset,
+                cont=continuation,
+                label=label,
+                parent=lastTag,
+            )
+        else:
+            parent = None
+            if label != tag:
+                if lastWord:
+                    if label.startswith("I-") and lastWord.label.startswith(
+                        ("B-", "I-")
+                    ):
+                        parent = lastTag
+                    elif label == lastWord.label and continuation:
+                        parent = lastWord
+                    else:
+                        parent = lastTag = Node(
+                            f"[{tag}]", type="tag", tag=tag, parent=root
+                        )
+                else:
+                    parent = lastTag
+            else:
+                if continuation and lastWord:
+                    parent = lastWord
+                else:
+                    parent = lastTag = Node(
+                        f"[{tag}]", type="tag", tag=tag, parent=root
+                    )
+
+            lastWord = Node(
+                f"{'##' if continuation else ''}{word} [{label}] [{pos + globalOffset}:{next + globalOffset}]",
+                type="word",
+                word=word,
+                s=pos + globalOffset,
+                e=next + globalOffset,
+                cont=continuation,
+                label=label,
+                parent=parent,
+            )
+    return root
+
+
+def walkTree(text, tree: Node, tags: Dict[str, str]):
+    annotated = []
+    interesting = []
+    offset = tree.globalOffset
+    for t in PreOrderIter(tree, maxlevel=2, filter_=lambda n: n.type == "tag"):
+        words = []
+        start, end = -1, -1
+        for w in PreOrderIter(t, filter_=lambda n: n.type == "word"):
+            words.append(text[offset : w.s])
+            if start == -1:
+                start = w.s
+            end = offset = w.e
+
+            words.append(w.word)
+
+        coloring = tags.get(t.tag)
+        if coloring:
+            subString = "".join(words)
+            tagged = (subString, t.tag, coloring)
+            # try:
+            #     index = s.index(subString, start - 1)
+            # except:
+            #     st.write(subString, start, end)
+            #     raise
+            # if index != start:
+            #     st.write(subString, "Expected:", start, "Actual:", index)
+            annotated.append(tagged)
+            interesting.append((tagged, (start, end)))
+        else:
+            annotated.append("".join(words))
+
+    return annotated, interesting
